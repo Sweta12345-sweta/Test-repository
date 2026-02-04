da2e537f748e6f66afc743a944a36727da995b6b
Sweta12345-sweta <s12196014@gmail.com>
2026-02-04 19:27:31 +0530

    test

--- CustomerManagement.API/Controllers/ProductController.cs
+++ CustomerManagement.API/Controllers/ProductController.cs
@@ -28,7 +28,7 @@ namespace CustomerManagement.API.Controllers
            return Ok(response.Data);
         }
 
-        [HttpGet()]
+        [HttpGet("{id}")]
         public async Task<IActionResult> GetById(int id)
         {
             var response = await _service.GetByIdAsync(id);
