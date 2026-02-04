410ee8926d04af658e30a6472d6de73d7dcb152c
Sweta12345-sweta <s12196014@gmail.com>
2026-02-04 19:12:44 +0530

    test commit

--- CustomerManagement.Infrastructure/obj/Debug/net8.0/CustomerManagement.Infrastructure.AssemblyInfo.cs
+++ CustomerManagement.Infrastructure/obj/Debug/net8.0/CustomerManagement.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CustomerManagement.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+671d0e704d4fa9a9c69e801db2c24ce41a61874c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CustomerManagement.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CustomerManagement.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
